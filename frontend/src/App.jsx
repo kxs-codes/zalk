da28de8cb264041050f945d98d1e0cba1295d82e
Ali <ah4100574@gmail.com>
2025-03-05 15:14:35 -0500

    feat(routes): created all routes to be used for the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,12 @@
-import {Routes, Route} from 'react-router-dom'
-import './App.css'
-import StudentList from './pages/StudentList.jsx';
-import Login from "./pages/Login.jsx";
-
-// TODO -> Change the routes to the `useRoutes` method so we can promote minimal code
+import './App.css';
+import AppRoutes from './Routes.jsx';
 
 
 function App() {  
   return (
-        <>
-                <Routes>
-                    <Route path ="/" element={<Login/>}/>
-                    <Route path = "/student_list" element={<StudentList/>}/>
-                </Routes>
-
-        </>
+      <>
+          <AppRoutes/>
+      </>
   );
 }
 
