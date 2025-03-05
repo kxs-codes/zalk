2b5c30cb2c82e0061c556bb5dae212e9e5d2e042
Kenny Smith <lifeisastage101@outlook.com>
2025-03-05 15:13:34 -0500

    feat: Made changes to the login styling to match the wireframe
    ! Need to finalize styling and give the official look that is required from stakeholder

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,14 +17,14 @@ function Login() {
     };
 
     return (
-        <div className=' h-screen w-screen bg-blue-400 '>
-                <h1 className=' '>Welcome Back! </h1>
-
+        <div className=' h-screen w-screen bg-white flex item-center justify-center'                                                  >
                 {/* TODO: Is there a better way to center content on the screen?*/}
-            <div className=' my-24'>
-            <form className=' bg-gray-500  p-6  w-100 h-100 rounded-2xl flex flex-col space-y-4 justify-self-center hover:shadow-2xl ' onSubmit={onSubmit}>
+
+            <div>
+            <div className='bg-gray-500 rounded-4xl mx-10 flex  items-center '        >
+            <form className='  py-40 bg-gray-500  p-6  w-100 h-100 rounded-2xl flex flex-col space-y-4 justify-self-center hover:shadow-2xl  ' onSubmit={onSubmit}>
+            <h1 className=' flex font-bold text-2xl'>Welcome Back!</h1>
                 <div className=' flex-col'>
-                
                     <label className='text-black text-2xl font-bold flex'>Username</label>
                     <input
                         type='text'
@@ -43,9 +43,11 @@ function Login() {
                         className='border-1 border-gray-300 w-full pb-4 p-2 rounded'
                     />
                 </div>
-                    <button type="submit" className='flex bg-red-900 text-white font-medium  hover:bg-amber-200 rounded-4xl py-3  justify-center'>Login</button>
+                <button type="submit" className='flex bg-red-900 text-white font-medium  hover:bg-amber-200 rounded-4xl py-3  justify-center'>Login</button>
             </form>
+            
             </div>
+            </div>  
             
         </div>
     );
