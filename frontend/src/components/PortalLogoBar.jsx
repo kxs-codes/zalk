1cd9e85126785c94f30d2fb6d7b37d35bf9f7350
Kenny Smith <lifeisastage101@outlook.com>
2025-03-17 14:49:17 -0400

    feat(EducatorPortal): implement EducatorPortal and StudentClassrooms pages; add dynamic role-based rendering in Portal component

--- frontend/src/components/PortalLogoBar.jsx
+++ frontend/src/components/PortalLogoBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // TODO: Implement dynamic Title, username, and logo image to show on the right side of the bar depending on the user logged in
 const PortalLogoBar = () => {
   return (
-    <div className="w-screen flex sm:ml-43.5 lg:ml-52 items-center shadow-2xl fixed top-0 left-0 bg-[#FAFAFA]">
+    <div className="w-screen flex ml-45 items-center shadow-2xl fixed top-0 left-0 bg-[#FAFAFA]">
         <p className="w-3/4 py-3 pl-5  rounded-r-full text-left font-medium text-black lg:text-4xl md:text-2xl">ZALK: Advanced Learning and Knowledge</p>
 
         {/* This is where we will add the logic for the user based on title and username */}
