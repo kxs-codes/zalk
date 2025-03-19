88e46dc569a9dfd4ac397f914d090e93698ce5cc
Ali <ah4100574@gmail.com>
2025-03-19 04:23:51 -0400

    fix: removed leftover tailwindcss in moderator create account react file

--- frontend/src/pages/Moderator/ModeratorCreateAccount.jsx
+++ frontend/src/pages/Moderator/ModeratorCreateAccount.jsx
@@ -23,13 +23,13 @@ const ModeratorCreateAccount = () => {
         <div className="account-container">
             <div className="inner-container">
                 <section className="account-section">
-                    <h2 className="section-header"><UserIcon className="size-6 text-green-500"/> Create User Account</h2>
+                    <h2 className="section-header"><UserIcon className="icon-styling"/> Create User Account</h2>
 
-                    <div className="w-full flex flex-col items-center justify-center p-2">
+                    <div className="form-container-styling">
                         <form className="form-tag" onSubmit={handleSubmit}>
                             <div className="input-container">
                                 <label htmlFor="account-type">Account Type</label>
-                                <select name="account-type" id="account-type" className="bg-white rounded-lg py-1" value={formData.accountType} onChange={handleChange}>
+                                <select name="account-type" id="account-type" className="select-account" value={formData.accountType} onChange={handleChange}>
                                     <option value="student">Student</option>
                                     <option value="educator">Educator</option>
                                     <option value="guardian">Guardian</option>
@@ -39,24 +39,24 @@ const ModeratorCreateAccount = () => {
                             
                             <div className="input-container">
                                 <label htmlFor="email">Email</label>
-                                <input type="email" id="email" name="email" placeholder="Enter Email Here..." className="bg-white rounded-lg px-2 py-1" value={formData.email} onChange={handleChange} />
+                                <input type="email" id="email" name="email" placeholder="Enter Email Here..." className="select-tags" value={formData.email} onChange={handleChange} />
                             </div>
 
                             <div className="input-container">
                                 <label htmlFor="username">Username</label>
-                                <input type="text" id="username" name="username" placeholder="Enter Username Here..." className="bg-white rounded-lg px-2 py-1" value={formData.username} onChange={handleChange} />
+                                <input type="text" id="username" name="username" placeholder="Enter Username Here..." className="select-tags" value={formData.username} onChange={handleChange} />
                             </div>
 
                             <div className="input-container">
                                 <label htmlFor="password">Password</label>
-                                <input type="password" id="password" name="password" placeholder="•••••••••" className="bg-white rounded-lg px-2 py-1" value={formData.password} onChange={handleChange} />
+                                <input type="password" id="password" name="password" placeholder="•••••••••" className="select-tags" value={formData.password} onChange={handleChange} />
                             </div>
 
                             <div className="input-container">
                                 <label htmlFor="confirm-password">Confirm Password</label>
-                                <input type="password" id="confirm-password" name="confirm-password" placeholder="•••••••••" className="bg-white rounded-lg px-2 py-1" value={formData.confirmPassword} onChange={handleChange}  />
+                                <input type="password" id="confirm-password" name="confirm-password" placeholder="•••••••••" className="select-tags" value={formData.confirmPassword} onChange={handleChange}  />
                             </div>
-                            <button type="submit" className="bg-dark-red-primary-1 hover:bg-red-primary-3 cursor-pointer text-white p-4 my-2 rounded-full">Sign Up</button>
+                            <button type="submit" className="button-styling">Sign Up</button>
                         </form>
                     </div>
                 </section>
