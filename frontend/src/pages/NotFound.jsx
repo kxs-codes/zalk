339955364df160b4a2ac273c9ddd8d792bed0c44
Luke Buczynski <lukebuczynski13@gmail.com>
2025-03-19 11:29:26 -0400

    refactor: Refactored NotFound and fixed commenting errors in multiple files

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
+import '../styles/NotFound.css';  // Import the CSS file
 
 const NotFound = () => {
     return(
-        <div className="w-screen h-screen flex flex-col items-center justify-center bg-yellow-50 bg-[url(/savannah.webp)]">
-            <h1 className="text-8xl mb-8 absolute top-20">404 Page Not Found</h1>
-            <div className="w-11/12 md:w-5/8 lg:w-1/2 flex flex-col bg-amber-200 text-green-700 shadow-lg rounded-lg p-8 text-2xl">
-                <p className="mb-2 ">Oops, looks like you've ventured into the outback! Here's a link to login to get you back on track so you don't get eaten by lions!</p>
-                <Link to='/' className="hover:underline">Login</Link>
+        <div className="not-found-container">
+            <h1 className="not-found-title">404 Page Not Found</h1>
+            <div className="not-found-box">
+                <p className="mb-2">Oops, looks like you've ventured into the savannah! Here's a link to login to get you back on track so you don't get eaten by lions!</p>
+                <Link to='/' className="not-found-link">Login</Link>
             </div>
         </div>
-    )
+    );
 }
 
 export default NotFound;
