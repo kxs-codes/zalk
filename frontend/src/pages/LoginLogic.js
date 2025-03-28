72a2b9e20bb650b5d5aa1a9b0c2c7438ace29337
Ali Hamad <115514687+ah410@users.noreply.github.com>
2025-03-28 00:06:50 -0400

    refactor: added js logic in the new file

--- frontend/src/pages/LoginLogic.js
+++ frontend/src/pages/LoginLogic.js
@@ -1,7 +1,32 @@
-// IMPORT REACT HOOKS THAT WILL BE USED
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const useLogin = (role, setRole) => {
-    // INSERT COPIED LOGIC HERE FROM YOUR .jsx FILE
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const onSubmit = (e) => {
+      e.preventDefault();
+      setUsername('');
+      setPassword('');
+      // Navigate to portal, passing role in state
+      navigate('/portal', { state: { role } });
+    };
+  
+    const handleChange = (e) => {
+      setRole(e.target.value);
+      console.log('role:', e.target.value);
+    };
+    
+    return {
+        username,
+        setUsername,
+        password,
+        setPassword,
+        onSubmit,
+        handleChange,
+    }
 }
 
 export default useLogin;
