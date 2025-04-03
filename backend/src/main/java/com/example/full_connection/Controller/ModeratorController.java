bc69550af57c09fbfa117b10cdcf8535963b3091
Ali <ah4100574@gmail.com>
2025-04-02 20:02:26 -0400

    feat: got backend endpoints working for manage accounts

--- backend/src/main/java/com/example/full_connection/Controller/ModeratorController.java
+++ backend/src/main/java/com/example/full_connection/Controller/ModeratorController.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +14,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.full_connection.DTO.AccountDTO;
 import com.example.full_connection.DTO.FormDataModerator;
 import com.example.full_connection.Entity.Classrooms;
 import com.example.full_connection.Service.ClassroomsService;
 import com.example.full_connection.Service.EducatorService;
+import com.example.full_connection.Service.ModeratorService;
 import com.example.full_connection.Service.StudentService;
 
 
@@ -34,6 +38,9 @@ public class ModeratorController {
 
     @Autowired
     private ClassroomsService classroomsService;
+
+    @Autowired
+    private ModeratorService moderatorService;
 
     @GetMapping("/students-and-educators")
     public ResponseEntity<Map<String, List<String>>> getStudentsAndEducators() {        
@@ -74,5 +81,29 @@ public class ModeratorController {
 
         // 3. Return response
         return ResponseEntity.ok("Classroom created successfully: " + savedClassroom.getClassId().toString());
+    }
+
+    @GetMapping("/grab-accounts")
+    public ResponseEntity<List<AccountDTO>> grabAccounts(@RequestParam String accountType) {
+        // 1. Verify the account type is an existing type
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList("student", "educator", "guardian", "advisory"));
+        if (!arrayList.contains(accountType.toLowerCase())) {
+            AccountDTO accountDTO = new AccountDTO("Account Type Not Valid");
+            List<AccountDTO> accounts = new ArrayList<>();
+            accounts.add(accountDTO);
+            return ResponseEntity.badRequest().body(accounts);
+        }
+
+        // 2. Send to the service to grab the List<AccountDTO> of that account type
+        List<AccountDTO> accounts = moderatorService.grabListAccounts(accountType);
+
+        // 3. Return to the frontend a ResponseEntity
+        return ResponseEntity.ok(accounts);
+    }
+
+    @PostMapping("/delete-account")
+    public ResponseEntity<String> deleteAccount(@RequestParam UUID id, @RequestParam String accountType) {
+        String message = moderatorService.removeAccount(id, accountType);
+        return ResponseEntity.ok(message);
     }
 }
--- backend/src/main/java/com/example/full_connection/Service/ModeratorService.java
+++ backend/src/main/java/com/example/full_connection/Service/ModeratorService.java
@@ -1,14 +1,118 @@
 package com.example.full_connection.Service;
 
+import java.security.Guard;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.full_connection.Repository.ModeratorRepository;
+import com.example.full_connection.DTO.AccountDTO;
+import com.example.full_connection.Entity.AdvisoryBoard;
+import com.example.full_connection.Entity.Educator;
+import com.example.full_connection.Entity.Guardian;
+import com.example.full_connection.Entity.Student;
+import com.example.full_connection.Repository.AdvisoryBoardRepository;
+import com.example.full_connection.Repository.EducatorRepository;
+import com.example.full_connection.Repository.GuardianRepository;
+import com.example.full_connection.Repository.StudentRepository;
 
 @Service
 public class ModeratorService {
     @Autowired
-    private ModeratorRepository moderatorRepository;
+    private StudentRepository studentRepository;
 
+    @Autowired
+    private EducatorRepository educatorRepository;
+
+    @Autowired
+    private GuardianRepository guardianRepository;
+
+    @Autowired
+    private AdvisoryBoardRepository advisoryBoardRepository;
+
+    public List<AccountDTO> grabListAccounts(String accountType) {
+        // 1. Create generic accounts List
+        List<?> accounts = new ArrayList<>();
+
+        // 2. Make a query to the right repository of the accountType for all accounts, returns account objects
+        switch(accountType.toLowerCase()) {
+            case "student":
+                accounts = studentRepository.findAll();
+                break;
+            case "educator":
+                accounts = educatorRepository.findAll();
+                break;
+            case "guardian":
+                accounts = guardianRepository.findAll();
+                break;
+            case "advisory_board":
+                accounts = advisoryBoardRepository.findAll();
+                break;
+            default:
+                return List.of(new AccountDTO("Invalid account type: " + accountType));
+        }
+
+        // 3. Convert each item in a list to a DTO object usint stream()
+        //      allowing to process collections of elements with things like filtering/mapping
+        //      typecast account to prevent generic constructor error
+        return accounts.stream()
+            .map(account -> {
+                if (account instanceof Student) return new AccountDTO((Student) account);
+                if (account instanceof Educator) return new AccountDTO((Educator) account);
+                if (account instanceof Guardian) return new AccountDTO((Guardian) account);
+                if (account instanceof AdvisoryBoard) return new AccountDTO((AdvisoryBoard) account);
+                return new AccountDTO("Unknown account type encountered.");
+            })
+            .collect(Collectors.toList());
+    }
+
+    public String removeAccount(UUID id, String accountType) {
+        switch(accountType.toLowerCase()) {
+            case "student":
+                Optional<Student> student = studentRepository.findById(id);
+
+                if (student.isPresent()) {
+                    studentRepository.delete(student.get());
+                } else {
+                    return "No student found with that id.";
+                }
+                break;
+            case "educator":
+                Optional<Educator> educator = educatorRepository.findById(id);
+
+                if (educator.isPresent()) {
+                    educatorRepository.delete(educator.get());
+                } else {
+                    return "No educator found with that id.";
+                }
+                break;
+            case "guardian":
+                Optional<Guardian> guardian = guardianRepository.findById(id);
+                
+                if (guardian.isPresent()) {
+                    guardianRepository.delete(guardian.get());
+                } else {
+                    return "No guardian found with that id.";
+                }
+                break;
+            case "advisory_board":
+                Optional<AdvisoryBoard> advisoryBoard = advisoryBoardRepository.findById(id);
+
+                if (advisoryBoard.isPresent()) {
+                    advisoryBoardRepository.delete(advisoryBoard.get());
+                } else {
+                    return "No advisory_board found with that id.";
+                }
+                break;
+            default:
+                return "No account type matching that deletion";
+        }
+
+        return "Deleted Successfully Account with id: " + id;
+    }
 
 }
