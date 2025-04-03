8cb1c8659cc395ef615263e935bf61fbbbba7ce8
Kenny Smith <lifeisastage101@outlook.com>
2025-04-03 08:29:59 -0400

    refactor: comment out educators relationship in Classrooms entity

--- backend/src/main/java/com/example/full_connection/Entity/Classrooms.java
+++ backend/src/main/java/com/example/full_connection/Entity/Classrooms.java
@@ -10,7 +10,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
@@ -32,8 +31,8 @@ public class Classrooms {
     
     private List<Student> students;
 
-    @ManyToMany
-    private List<Educator> educators;
+    // @ManyToMany
+    // private List<Educator> educators;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "educator_id", referencedColumnName = "educator_id", nullable = false)
@@ -58,9 +57,9 @@ public class Classrooms {
     public Educator getEducator() {
         return educator;
     }
-    public List<Educator> getEducators() {
-        return educators;
-    }
+    // public List<Educator> getEducators() {
+    //     return educators;
+    // }
     
     public String getSubjectLevel() {
         return subjectLevel;
@@ -82,7 +81,7 @@ public class Classrooms {
     public void setSubjectLevel(String subjectLevel) {
         this.subjectLevel = subjectLevel;
     }
-    public void setEducators(List<Educator> educators) {
-        this.educators = educators;
-    }
+    // public void setEducators(List<Educator> educators) {
+    //     this.educators = educators;
+    // }
 }
