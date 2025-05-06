40ce84375cc1d30ad797295b33d7da28858d24c7
Ali <ah4100574@gmail.com>
2025-05-06 02:27:10 -0400

    refactor: improved logic flow in the skeleton for retraining the model.

--- backend/src/main/java/com/example/full_connection/Model/ModelSaveAndLoad.java
+++ backend/src/main/java/com/example/full_connection/Model/ModelSaveAndLoad.java
@@ -7,9 +7,18 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.full_connection.Entity.Statistics;
 import com.example.full_connection.Model.RandomForestRegressor;
+import com.example.full_connection.Repository.StatisticsRepository;
+
+import smile.data.DataFrame;
 
 public class ModelSaveAndLoad {
+    @Autowired
+    StatisticsRepository statisticsRepository;
+
     public void saveModel(RandomForestRegressor model, String filePath) {
         try {
             // 1. Write the forest to a file
@@ -48,15 +57,20 @@ public class ModelSaveAndLoad {
     // TODO: Retrain function (make it thread-safe)
     public RandomForestRegressor retrainModel() {
         // 1. Grab all student statistics (all rows in the table)
+        List<Statistics> allStats = statisticsRepository.findAll();
 
-        // 2. Convert into a csv file
+        // 2. Convert to a DataFrame (might be able to skip step 2 and convert directly)
+            // Make schema
+            // Make empty List of Tuples
+            // Loop over all stats, making a tuple and adding to tuple list
+            // Create DataFrame passing in the schema and List<Tuple>
 
-        // 3. Convert to a DataFrame (might be able to skip step 2 and convert directly)
+        // 3. Instantiate a new model of class RandomForestRegressor
+        RandomForestRegressor model = new RandomForestRegressor(200, 5, 10);
 
-        // 4. Instantiate a new model of class RandomForestRegressor
+        // 4. Train the model using the fit() function, passing in the DataFrame of stats
 
-        // 5. Train the model using the fit() function, passing in the DataFrame of stats
-
-        // 6. Return the model
+        // 5. Return the model
+        return model;
     }
 }
